fdaa961f7b079c10b8604b7a3583aba3bb5c4111
Ralica Popova <ralicaterzieva90@gmail.com>
2024-09-06 18:21:30 +0300

    Add react navigation logic

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer  } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import AllPlaces from './screens/AllPlaces';
+import AddPlace from './screens/AddPlace';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <>
+    <StatusBar style='dark' />
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="AllPlaces" component={AllPlaces}/>
+        <Stack.Screen name="AddPlace" component={AddPlace}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+   </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/Places/PlaceForm.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+function PlaceForm() {
+    return <View>
+        <Text>The Place Form</Text>
+    </View>
+}
+
+export default PlaceForm;
