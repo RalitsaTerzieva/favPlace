d034a10a65e159d5d654d9aa240732cf412737f0
Ralica Popova <ralicaterzieva90@gmail.com>
2024-09-07 18:35:48 +0300

    Add photo Ios logic and permissions

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -1,8 +1,32 @@
-import { View, Button } from 'react-native';
-import { launchCameraAsync } from 'expo-image-picker';
+import { View, Button, Alert } from 'react-native';
+import { launchCameraAsync, useCameraPermissions, PermissionStatus } from 'expo-image-picker';
 
 function ImagePicker() {
+    const [cameraPermission, requestCameraPermissionStatus] = useCameraPermissions();
+
+    async function verifyPermissions() {
+        if(cameraPermission.status === PermissionStatus.UNDETERMINED) {
+            const permissionResponse = await requestCameraPermissionStatus();
+
+            return permissionResponse.granted;
+        }
+
+        if(cameraPermission.status === PermissionStatus.DENIED) {
+            Alert.alert('Insufficient permissions! You need to grant a camera permission to use this app.');
+
+            return false;
+        }
+
+        return true;
+    }
+
     async function takeImageHandler() {
+        const hasPermission = await verifyPermissions();
+
+        if(!hasPermission) {
+            return;
+        }
+
         const image = await launchCameraAsync({
             allowsEditing: true,
             aspect: [16, 9],
