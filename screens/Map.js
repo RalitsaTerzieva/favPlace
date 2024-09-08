d0f2498ff7c4d0d51ef21a396e5b4ca9bf7a9752
Ralica Popova <ralicaterzieva90@gmail.com>
2024-09-08 19:26:59 +0300

    Add new Icon button

--- screens/Map.js
+++ screens/Map.js
@@ -1,8 +1,9 @@
 import MapView, { Marker } from 'react-native-maps';
-import { StyleSheet } from 'react-native';
-import { useState } from 'react';
+import { StyleSheet, Alert } from 'react-native';
+import { useState, useLayoutEffect, useCallback } from 'react';
+import IconButton from '../components/UI/IconButton';
 
-function Map() {
+function Map({navigation}) {
     const [selectedLocation, setSelectedLocation] = useState();
 
     const region = {
@@ -18,6 +19,21 @@ function Map() {
 
         setSelectedLocation({lat: lat, lng: lng})
     }
+
+    const savedPickedLocationHandler = useCallback(() => {
+        if(!selectedLocation) {
+            Alert.alert('No location picked! You have to pick a location by tapping on the map first.');
+            return;
+        }
+
+        navigation.navigate('AddPlace', { pickedLat: selectedLocation.lat, pickedLng: selectedLocation.lng })
+    }, [navigation, selectedLocation])
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerRight: ({tintColor}) => <IconButton icon="save" size={24} color={tintColor} onPress={savedPickedLocationHandler}/>
+        });
+    }, [navigation, savedPickedLocationHandler])
 
     return (
         <MapView style={styles.map} initialRegion={region} onPress={selectLocationHandler}>
