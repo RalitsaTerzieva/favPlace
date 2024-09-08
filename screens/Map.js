dbe7ea492cdd0d6f9efc1c08921bc4b51103ba27
Ralica Popova <ralicaterzieva90@gmail.com>
2024-09-08 19:14:48 +0300

    Add marker and selected location logic

--- screens/Map.js
+++ screens/Map.js
@@ -1,7 +1,10 @@
 import MapView, { Marker } from 'react-native-maps';
 import { StyleSheet } from 'react-native';
+import { useState } from 'react';
 
 function Map() {
+    const [selectedLocation, setSelectedLocation] = useState();
+
     const region = {
         latitude:  37.78,
         longitude: -122.43,
@@ -9,7 +12,18 @@ function Map() {
         longitudeDelta: 0.0421,
     }
 
-    return <MapView style={styles.map} initialRegion={region} ></MapView>
+    function selectLocationHandler(event) {
+        const lat = event.nativeEvent.coordinate.latitude;
+        const lng = event.nativeEvent.coordinate.longitude;
+
+        setSelectedLocation({lat: lat, lng: lng})
+    }
+
+    return (
+        <MapView style={styles.map} initialRegion={region} onPress={selectLocationHandler}>
+            {selectedLocation && <Marker title="Picked Location" coordinate={{latitude: selectedLocation.lat, longitude: selectedLocation.lng}}/>}
+        </MapView>
+    )
 }
 
 export default Map;
